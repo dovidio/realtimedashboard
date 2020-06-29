32414daafe5ebe45e6227ee6ed76f9a1f56afa85
Umberto D'Ovidio <umberto.dovidio@gmail.com>
2020-06-29 02:14:16 +0200

    add server and angular to docker compose

--- docker.sh
+++ docker.sh
@@ -1,8 +1,27 @@
 docker-compose up -d mongodb
 
-sleep 5
+echo "waiting for mongodb to be up and running"
 
-# setup single node replica set
-docker exec -it mongodb mongo --eval "rs.initiate({_id : 'rs0', members: [{ _id : 0, host : \"mongodb:27017\" }]});rs.slaveOk(); db.getMongo().setReadPref('nearest');db.getMongo().setSlaveOk();"
+is_healthy() {
+    service="$1"
+    container_id="$(docker-compose ps -q "$service")"
+    health_status="$(docker inspect -f "{{.State.Status}}" "$container_id")"
 
-exit
+    if [ "$health_status" = "running" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+while ! is_healthy  mongodb; do sleep 1; done
+
+echo "setting up mongo replica set"
+until docker exec mongodb mongo --eval "rs.initiate({_id : 'rs0', members: [{ _id : 0, host : \"mongodb:27017\" }]});rs.slaveOk(); db.getMongo().setReadPref('nearest');db.getMongo().setSlaveOk();"
+do
+    echo ...
+    sleep 1
+done
+
+# running the other services
+docker-compose up -d
