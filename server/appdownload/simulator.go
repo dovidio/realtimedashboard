1beb48e1c613b7ba5c8d87bbb89d8b9e38eef72d
Umberto D'Ovidio <umberto.dovidio@gmail.com>
2020-06-29 22:28:51 +0200

    improve simulation data

--- server/appdownload/simulator.go
+++ server/appdownload/simulator.go
@@ -1,6 +1,7 @@
 package appdownload
 
 import (
+	"math"
 	"math/rand"
 	"time"
 )
@@ -22,6 +23,11 @@ var countries = [...]string{
 	"Austria",
 	"Switzerland",
 	"Spain",
+	"Sudan",
+	"Russia",
+	"Australia",
+	"Brasil",
+	"Argentina",
 }
 
 // GenerateData insert a random app periodically with the specified interval
@@ -34,12 +40,56 @@ func GenerateData(interval time.Duration, repository Repository, quit chan struc
 			time.Sleep(interval)
 			var appDownload AppDownload
 			appDownload.AppID = AppNames[rand.Int31n(int32(len(AppNames)))]
-			appDownload.Latitude = rand.Float64()*20 + 40.0
-			appDownload.Longitude = rand.Float64() * 35
+			latitude, longitude := getRandomCoordinates()
+			appDownload.Latitude = latitude
+			appDownload.Longitude = longitude
 			appDownload.DownloadedAt = time.Now().UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond))
 			appDownload.Country = countries[rand.Int31n(int32(len(countries)))]
 
 			repository.Add(appDownload)
 		}
 	}
+}
+
+// returns random latitude and longitude
+func getRandomCoordinates() (float64, float64) {
+
+	var radius float64
+	var circleX float64
+	var circleY float64
+
+	switch rand.Intn(6) {
+	case 0: // Europea
+		radius = 5.0
+		circleX = 47.0
+		circleY = 8.0
+	case 1: // Asia
+		radius = 18.0
+		circleX = 56.0
+		circleY = 86.0
+	case 2: // Africa
+		radius = 12.0
+		circleX = 17.0
+		circleY = 7.0
+	case 3: // USA
+		radius = 13.0
+		circleX = 43.0
+		circleY = -102.0
+	case 4: // Australia
+		radius = 8.0
+		circleX = -23
+		circleY = 123
+	case 5: // South America
+		radius = 10.0
+		circleX = -11
+		circleY = -58
+	}
+
+	alpha := 2 * math.Pi * rand.Float64()
+	r := radius * math.Sqrt(rand.Float64())
+
+	x := r*math.Cos(alpha) + circleX
+	y := r*math.Sin(alpha) + circleY
+
+	return x, y
 }
